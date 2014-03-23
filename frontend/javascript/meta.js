9d535caecc932a17ffdb2fdfc14927607ddc2daa
Eric Florenzano <floguy@gmail.com>
2014-03-23 02:26:13 -0700

    Simplify

--- frontend/javascript/meta.js
+++ frontend/javascript/meta.js
@@ -1,20 +1,14 @@
 var emoji = require('./emoji');
 
 function getDetailMeta(im, url) {
-  var orig = ('https://' + process.env.AWS_S3_BUCKET_NAME +
+  var pic = ('https://' + process.env.AWS_S3_BUCKET_NAME +
     '.s3.amazonaws.com/' + im.picture);
-  /*
-  var resized = orig
-    .replace('original', 'resized')
-    .replace(/\..{1,5}$/, '.jpg')
-    .replace('.jpg', '/1000.jpg');
-  */
   var metaTags = [
     '<meta name="twitter:card" content="photo">',
     '<meta name="twitter:site" content="@irlmoji">',
     '<meta name="twitter:creator" content="@' + im.user.username + '">',
-    '<meta name="twitter:image" content="' + orig + '">',
-    '<meta name="twitter:title" content="' + emoji.EMOJI_DATA[im.emoji][0] + ' ' + url + ' #IRLMoji">',
+    '<meta name="twitter:image" content="' + pic + '">',
+    '<meta name="twitter:title" content="' + emoji.EMOJI_DATA[im.emoji][0] + '">',
   ];
   return metaTags.join('\n');
 }
