23b04f944b782ee245784d626d1543c83c01be66
Eric Florenzano <floguy@gmail.com>
2014-03-27 00:18:43 -0700

    Start to flesh out the about page.

--- frontend/javascript/components/basic.js
+++ frontend/javascript/components/basic.js
@@ -13,6 +13,10 @@ var About = React.createClass({
         <Header app={this.props.app} user={this.props.user} />
         <div className="container">
           <h2>IRLMoji is a place to post pictures that look like Emoji!</h2>
+          <p>
+            However, it is also a demo of how to set up a React.js site that
+            renders both on the server and the client.
+          </p>
         </div>
       </div>
     );
